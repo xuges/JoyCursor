511186a61d582e081c7b536574ded8e8b1baca9c
xuges <xuges@qq.com>
2024-04-28 12:37:09 +0800

    [project] 添加VS工程文件。Add VS project file.

---
+++ project/resource.h
@@ -0,0 +1,19 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 JoyCursor.rc 使用
+//
+#define IDI_ICON                        106
+#define IDS_STRING_ABOUT                107
+#define IDS_STRING_MENU_ABOUT           110
+#define IDS_STRING_MENU_EXIT            111
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        112
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
